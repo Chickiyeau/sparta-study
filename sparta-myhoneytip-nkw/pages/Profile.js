d6c693dc36632ba7119df1e9fcabdecc902985e3
chickiyeah <55694044+chickiyeah@users.noreply.github.com>
2022-06-29 22:55:04 +0900

    ahh

--- sparta-myhoneytip-nkw/pages/Profile.js
+++ sparta-myhoneytip-nkw/pages/Profile.js
@@ -1,45 +1,81 @@
-import * as React from 'react';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, Linking} from 'react-native';
-import * as Notifications from 'expo-notifications';
-import KakaoLogin from '../LoginScreen/kakao/kakaoLogin';
+import React, {createContext, useContext, useReducer} from 'react';
+
+import { StyleSheet,Text,View,Button, Alert, Image, ScrollView, D} from "react-native";
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Fragment } from 'react/cjs/react.production.min.js';
+
+import '../global.js'
+
 
 export default function ProfileScreen({navigation}) {
-   return (
-<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-<TouchableOpacity style={styles.aboutButton} onPress={() => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "버튼을 눌러 알림 발생",
-        body: '버튼을 눌러 알림을 생성했습니다!',
-      },
-      trigger: {
-        seconds: 1, //onPress가 클릭이 되면 60초 뒤에 알람이 발생합니다.
-      },
-    });
-  }}>
-          <Text style={styles.aboutButtonText}>눌러서 알림 발생</Text>
-        </TouchableOpacity>
+    return (
+        <View style={styles.container}>
+            <View style={styles.titlecontainer}>
+                <Text style={styles.title}>내 정보</Text>
+            </View>
 
-        <TouchableOpacity style={styles.aboutButton} onPress={() => {navigation.navigate('kakaoLogin')}}>
-          <Text style={styles.aboutButtonText}>눌러서 스파르타 이동 알림</Text>
-        </TouchableOpacity>
-</View>
-   );   
- }
+            <ScrollView style={styles.desccontainer}>
+                <View style={styles.topcontainer1}>
+                    <View style={styles.profileimage}>
+                        
+                    </View>
+                    <Fragment>
+                    <div style={styles.namecontainer}></div>
+                    </Fragment>
+                    
+                </View>
+            </ScrollView>
 
- const styles = StyleSheet.create({
-    aboutButton: {
-        backgroundColor:"pink",
-        width:100,
-        height:40,
-        borderRadius:10,
-        alignSelf:"flex-end",
-        marginRight:20,
-        marginTop:10
-      },
-      aboutButtonText: {
-        color:"#fff",
-        textAlign:"center",
-        marginTop:10
-      }
- })
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor:"blue"
+    },
+    titlecontainer: {
+        width:300,
+        height:20,
+        backgroundColor:"yellow",
+        alignSelf:"center",
+        marginTop:20,
+        alignContent:"center",
+        borderRadius:20
+    },
+    title: {
+       fontSize:15,
+       textAlign:'center' 
+    },
+    //본문
+    desccontainer: {
+        width:350,
+        height:640,
+        marginTop:10,
+        marginLeft:17.5,
+        backgroundColor:"green"
+    },
+    topcontainer1:{
+        height:100,
+        backgroundColor:"magenta"
+    },
+    //프로필이미지
+    profileimage:{
+        witdh:30,
+        height:50,
+        marginLeft:5,
+        marginTop:5,
+        marginBottom:5,
+        marginRight:280,
+        backgroundColor:"orange"
+    },
+    //이름
+    namecontainer:{
+        witdh:30,
+        height:30,
+        marginLeft:120,
+        backgroundColor:"black"
+    }
+})
